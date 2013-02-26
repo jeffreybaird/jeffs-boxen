b1ac21ffdbbc8651ff57e659a154b3ec920ed645
jeffreybaird <jbaird@medivo.com>
2013-02-26 10:23:56 -0500

    added redis

--- manifests/site.pp
+++ manifests/site.pp
@@ -56,7 +56,7 @@ node default {
   include nginx
   include nvm
   include ruby
-  include mysql
+
 
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
@@ -90,6 +90,8 @@ node default {
 
 # databases
 
-#  include mysql
+  include mysql
+  mysql::db { 'mydb': }
+  include redis
 
-mysql::db { 'mydb': }
+
